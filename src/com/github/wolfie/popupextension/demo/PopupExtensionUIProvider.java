f9dda42f8ffd9eeee0d28da7f307c43452c8257d
Henrik Paul <henrik.paul@gmail.com>
2013-04-22 22:07:44 +0300

    Added test case for issue #2

--- src/com/github/wolfie/popupextension/demo/PopupExtensionUIProvider.java
+++ src/com/github/wolfie/popupextension/demo/PopupExtensionUIProvider.java
@@ -1,6 +1,7 @@
 package com.github.wolfie.popupextension.demo;
 
 import com.github.wolfie.popupextension.test1.PopupExtensionTest1UI;
+import com.github.wolfie.popupextension.test2.PopupExtensionTest2UI;
 import com.vaadin.server.UIClassSelectionEvent;
 import com.vaadin.server.UIProvider;
 import com.vaadin.ui.UI;
@@ -19,6 +20,8 @@ public class PopupExtensionUIProvider extends UIProvider {
 
 		if (path.startsWith("/test1")) {
 			return PopupExtensionTest1UI.class;
+		} else if (path.startsWith("/test2")) {
+			return PopupExtensionTest2UI.class;
 		} else {
 			return DEFAULT_VIEW;
 		}
